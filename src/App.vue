2d59ed7620e29fee38f00c106dfa40c252a297ef
reiki-suda <reiki.suda@upstairs.jp>
2021-05-31 20:14:29 +0900

    Implement probabilities to excavate tuners

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,136 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <label>デッキ枚数<input type="number" min="0" v-model.number="deckSize"></label>
+    <label>デッキ内のチューナーの枚数<input type="number" min="0" v-model.number="deckTunerSize"></label>
+
+    <table>
+      <caption>チューナーをめくる確率</caption>
+      <thead>
+        <tr>
+          <th class="text-center">枚数</th>
+          <th class="text-right">確率（%）</th>
+          <th class="text-right">累積確率（%）</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="n in [5, 4, 3, 2, 1, 0]" :key="n">
+          <th class="text-center">{{ n }}枚</th>
+          <td class="text-right">{{ formatProbabilityOf(n) }}</td>
+          <td class="text-right">{{ formatCumulativeProbabilityOf(n) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { combinations } from 'mathjs'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      // Shooting Star Dragon excavates top 5 cards of your deck.
+      excavationSize: 5,
+      deckSize: 15,
+      deckTunerSize: 7,
+      precision: 1,
+    };
+  },
+  computed: {
+    denominator() {
+      return combinations(this.deckSize, this.excavationSize);
+    }
+  },
+  methods: {
+    probabilityOf(excavatedTunerSize) {
+      const deckNonTunerSize = this.deckSize - this.deckTunerSize;
+      const excavatedNonTunerSize = this.excavationSize - excavatedTunerSize;
+
+      // Invalid input patterns
+      if (
+        excavatedTunerSize < 0 ||
+        excavatedTunerSize > this.excavationSize ||
+        this.deckSize <= 0 ||
+        this.deckTunerSize <= 0 ||
+        this.deckSize < this.deckTunerSize ||
+        this.deckSize < this.excavationSize ||
+        deckNonTunerSize < excavatedNonTunerSize
+      ) {
+        return 0;
+      }
+
+      const tuner = combinations(this.deckTunerSize, excavatedTunerSize);
+      const nonTuner = combinations(deckNonTunerSize, excavatedNonTunerSize);
+      return tuner * nonTuner / this.denominator;
+    },
+    cumulativeProbabilityOf(minimumExcavatedTunerSize) {
+      let cumulativeProbability = 0;
+
+      for (let excavatedTunerSize = minimumExcavatedTunerSize; excavatedTunerSize <= this.excavationSize; excavatedTunerSize++) {
+        cumulativeProbability += this.probabilityOf(excavatedTunerSize);
+      }
+
+      return cumulativeProbability;
+    },
+    formatProbabilityOf(excavatedTunerSize) {
+      return (this.probabilityOf(excavatedTunerSize) * 100).toFixed(this.precision);
+    },
+    formatCumulativeProbabilityOf(minimumExcavatedTunerSize) {
+      return (this.cumulativeProbabilityOf(minimumExcavatedTunerSize) * 100).toFixed(this.precision);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+}
+input {
+  box-sizing: border-box;
+  width: 100%;
+  font-size: 16px;
+  text-align: right;
+  margin: 4px 0;
+  padding: 4px;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+table caption {
+  margin: 10px 0;
+}
+thead tr {
+  border-bottom: 1px solid #333;
+}
+tbody tr:nth-child(odd) {
+  background: #e3ecf5;
+}
+th {
+  padding: 0 10px;
+}
+td {
+  padding: 4px 30px;
+}
+p {
+  font-size: 14px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 20px;
+}
+
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 8px;
+}
+.text-center {
+  text-align: center;
+}
+.text-right {
+  text-align: right;
 }
 </style>
